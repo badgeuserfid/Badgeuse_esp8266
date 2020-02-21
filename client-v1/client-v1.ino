93996191cf77d222dd99e2bdb177b23b1fe3736d
Lenz Filipski <lenz.filipski@etu.umontpellier.fr>
2020-02-21 20:27:52 +0100

    added elements in client-v1 to use mqtt and json

--- client-v1/client-v1.ino
+++ client-v1/client-v1.ino
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#include <PubSubClient.h>
 #include <SPI.h>
 #include <MFRC522.h>
 #include <ArduinoJson.h>
@@ -15,6 +16,13 @@
 // Set the Wifi ssid and password to connect to
 const char* ssid = STASSID;
 const char* password = STAPSK;
+// Set the mqtt server to connect to
+const char* mqtt_server = "";
+const int mqtt_port = 1883;
+
+// Define the wifi client and mqtt Classes
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 // Create MFRC522 instance
 MFRC522 mfrc522(SS_PIN, RST_PIN);
@@ -24,6 +32,39 @@ StaticJsonDocument<200> doc;
 
 // Request number increments for every new request send
 unsigned int request = 0;
+
+// Delay without delay
+unsigned long currentMillis;
+unsigned long previousMillis = 0;
+unsigned long mqttMillis = 0;
+
+
+// callback for subscribed mqtt topics
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived ["); // DEBUG
+  Serial.print(topic); // DEBUG
+  Serial.print("] "); // DEBUG
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]); // DEBUG
+  }
+  Serial.println(); // DEBUG
+  /*
+   * Code to execute
+   */
+}
+
+void reconnect() {
+  if (currentMillis - mqttMillis > 5000) { // may work ??
+    if (client.connect("")) { // Insert client name
+      client.publish("esptopic", "newconnexion");
+      client.subscribe("servtopic");
+    } else {
+      // make somethings with the leds
+    }
+    mqttMillis = currentMillis;
+  }
+}
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -50,16 +91,30 @@ void setup() {
   Serial.println(); // DEBUG
   Serial.println("Connected!"); // DEBUG
   Serial.println(); // DEBUG
+
+  // Create the connexion to the mqtt server
+  client.setServer(mqtt_server, mqtt_port);
+  // set the callback function for subscribed topics
+  client.setCallback(callback);
   
 }
 
+
 void loop() {
   // put your main code here, to run repeatedly:
+  currentMillis = millis(); // Dw/oD
 
   // Check if wifi is still connected
   if (WiFi.status() != WL_CONNECTED) {
     ESP.restart();
   }
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  
+  // allow client to process incoming messages and maintain connexion
+  client.loop();
 
   // If card is detected and readable
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
@@ -86,6 +141,8 @@ void loop() {
       toSend["request"] = request; // generate a request number
       toSend["uid"] = uid; // the RFID tag UID to be send
       toSend["token"] = "yay"; // put esp token here
+      serializeJson(toSend, msg);
+      client.publish("esptopic", msg);
       
     }
   }
